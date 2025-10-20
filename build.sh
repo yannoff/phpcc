yannoff/phpcc

commit 205e3cb32f080b9b5c105685635bfd1ca6dcd75e

Builder: hide composer deprecation warnings

build.sh
@@ -24,7 +24,7 @@ then
 fi
 
 # Ensure dependencies are up-to-date
-offenbach install --no-dev --no-interaction --optimize-autoloader
+php -d error_reporting=1 offenbach install --no-dev --no-interaction --optimize-autoloader
 
 # Update version in main application bootstrap file
 sed -i "s/\$VERSION *=.*/\$VERSION = '$version';/" $main

commit ab106bbed1dd8835ad5deeb1f15b0b920c75b5dd

Builder: Fix `offenbach` install call
- Fix regression introduced in 205e3cb32f080b9b5c105685635bfd1ca6dcd75e
- Manually remove composer deprecation warnings

build.sh
@@ -24,7 +24,7 @@ then
 fi
 
 # Ensure dependencies are up-to-date
-php -d error_reporting=1 offenbach install --no-dev --no-interaction --optimize-autoloader
+offenbach install --no-dev --no-interaction --optimize-autoloader | awk '!/Deprec/' | tr -d "\r"
 
 # Update version in main application bootstrap file
 sed -i "s/\$VERSION *=.*/\$VERSION = '$version';/" $main